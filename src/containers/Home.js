67a2621cb95aa43034d941fc096d31ea99c3c23e
Alex Fernandez <63-alex.fernandez@users.noreply.gitlab.whiteskylabs.com>
2021-01-02 20:29:58 +1100

    Update css

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -5,6 +5,8 @@ import { API } from "aws-amplify";
 import { LinkContainer } from "react-router-bootstrap";
 import { BsPencilSquare } from "react-icons/bs";
 import { onError } from "../libs/errorLib";
+import { Link } from "react-router-dom";
+
 import "./Home.css";
 
 export default function Home() {
@@ -58,6 +60,14 @@ export default function Home() {
       <div className="lander">
         <h1>Scratch</h1>
         <p className="text-muted">A simple note taking app</p>
+        <div className="pt-3">
+          <Link to="/login" className="btn btn-info btn-lg mr-3">
+            Login
+          </Link>
+          <Link to="/signup" className="btn btn-success btn-lg mr-3">
+            Signup
+          </Link>
+        </div>
       </div>
     );
   };
